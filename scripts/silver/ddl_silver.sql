1d4e94bc08a260e6807cad3d4d6422d7ecea6b73
Thiramdasu Shivasharan <213768851+Shiva-Sharan@users.noreply.github.com>
2026-06-07 21:52:43 +0530

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -41,7 +41,7 @@ CREATE TABLE silver.crm_cust_info (
     cst_key              VARCHAR(50),
     cst_firstname        VARCHAR(50),
     cst_lastname         VARCHAR(50),
-    cst_marital_status   VARCHAR(50),
+    cst_martial_status   VARCHAR(50),
     cst_gndr             VARCHAR(50),
     cst_create_date      DATE,
     dwh_create_date      TIMESTAMP DEFAULT CURRENT_TIMESTAMP
