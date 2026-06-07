d3c683d0de576b58906c22151971c4ee46c04255
Thiramdasu Shivasharan <213768851+Shiva-Sharan@users.noreply.github.com>
2026-06-07 21:51:11 +0530

    Update ddl_table_bronze.sql

--- scripts/bronze/ddl_table_bronze.sql
+++ scripts/bronze/ddl_table_bronze.sql
@@ -39,7 +39,7 @@ CREATE TABLE bronze.crm_cust_info (
     cst_key             VARCHAR(50),
     cst_firstname       VARCHAR(50),
     cst_lastname        VARCHAR(50),
-    cst_marital_status  VARCHAR(50),
+    cst_martial_status  VARCHAR(50),
     cst_gndr            VARCHAR(50),
     cst_create_date     DATE
 );
