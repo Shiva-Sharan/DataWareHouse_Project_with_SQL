b87943176d25d41a5af68227284caa16e29e4746
Thiramdasu Shivasharan <213768851+Shiva-Sharan@users.noreply.github.com>
2026-06-08 12:15:31 +0530

    Update quality_checks.sql

--- scripts/bronze/quality_checks.sql
+++ scripts/bronze/quality_checks.sql
@@ -44,3 +44,59 @@ order by prd_start_dt) - interval '1 day' as prd_end_dt,
 prd_end_dt
 from bronze.crm_prd_info;
 
+select 
+nullif(sls_order_dt,0) as sls_order_dt
+from bronze.crm_sales_details
+where sls_order_dt <= 0 or char_length(sls_order_dt:: text) != 8 
+or sls_order_dt > 20500101 or sls_order_dt < 19000101
+
+select 
+nullif(sls_ship_dt,0) as sls_ship_dt
+from bronze.crm_sales_details
+where sls_ship_dt <= 0 or char_length(sls_ship_dt:: text) != 8 
+or sls_ship_dt > 20500101 or sls_ship_dt < 19000101
+
+
+select 
+nullif(sls_due_dt,0) as sls_due_dt
+from bronze.crm_sales_details
+where sls_due_dt <= 0 or char_length(sls_due_dt:: text) != 8 
+or sls_due_dt > 20500101 or sls_due_dt < 19000101
+
+select * 
+from bronze.crm_sales_details
+where sls_ship_dt < sls_order_dt or sls_order_dt > sls_due_dt
+
+select sls_sales, sls_quantity, sls_price 
+from bronze.crm_sales_details
+where sls_sales != sls_quantity * sls_price 
+or sls_sales is null or sls_quantity is null or sls_price is null
+or sls_sales <= 0 or sls_quantity <=0 or sls_price <= 0
+order by sls_sales, sls_quantity, sls_price 
+
+select abs(sls_sales)
+from bronze.crm_sales_details
+where sls_sales != sls_quantity * sls_price 
+or sls_sales is null or sls_quantity is null or sls_price is null
+or sls_sales <= 0 or sls_quantity <=0 or sls_price <= 0
+order by sls_sales, sls_quantity, sls_price 
+
+select sls_sales as sls_sold, sls_quantity, sls_price as sls_pold,
+case 
+	when sls_sales is null 
+	or sls_sales <=0 
+	or sls_sales = sls_quantity * abs(sls_price)
+	then sls_quantity * abs(sls_price)
+	else sls_sales
+end	as sls_sales, 
+sls_quantity, 
+case 
+	when sls_price is null
+	or sls_price <= 0 
+	then sls_sales / nullif(sls_quantity,0)
+	else sls_price
+end as sls_price 
+from bronze.crm_sales_details
+where sls_sales != sls_quantity * sls_price 
+or sls_sales is null or sls_quantity is null or sls_price is null
+or sls_sales <= 0 or sls_quantity <=0 or sls_price <= 0
