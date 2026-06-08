d61b3a12bbabc967fe47465e5854e296ade2db7f
Thiramdasu Shivasharan <213768851+Shiva-Sharan@users.noreply.github.com>
2026-06-08 10:27:14 +0530

    Update data_load_to_silver.sql

--- scripts/silver/data_load_to_silver.sql
+++ scripts/silver/data_load_to_silver.sql
@@ -41,3 +41,33 @@ FROM (
 ) t
 
 WHERE flag_test = 1;
+
+INSERT INTO silver.crm_prd_info(
+prd_id, 
+cat_id, 
+prd_key, 
+prd_nm, 
+prd_cost, 
+prd_line,
+prd_start_dt, 
+prd_end_dt
+)
+
+select 
+prd_id,
+cast(replace(substring(prd_key,1,5),'-','_') as Varchar(50)) as cat_id,
+cast(substring(prd_key,7, length(prd_key)) as varchar(50)) as prd_key,
+prd_nm, 
+coalesce(prd_cost, 0) as prd_cost,
+case upper(trim(prd_line))
+	when 'M' then 'Mountain'
+	when 'R' then 'Road'
+	when 'S' then 'Other Sales'
+	when 'T' then 'Touring'
+	else 'n/a'
+end as prd_line,
+cast(prd_start_dt as date) as prd_start_dt,
+cast(lead(prd_start_dt) over(
+partition by prd_key order by prd_start_dt
+) - interval '1 day' as date) as prd_end_dt
+from bronze.crm_prd_info;
