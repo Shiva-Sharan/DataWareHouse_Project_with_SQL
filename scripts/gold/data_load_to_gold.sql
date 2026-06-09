74fa789e6fd8348a187ed4991a6fa29435f63c0d
Thiramdasu Shivasharan <213768851+Shiva-Sharan@users.noreply.github.com>
2026-06-09 17:32:59 +0530

    Update data_load_to_gold.sql

--- scripts/gold/data_load_to_gold.sql
+++ scripts/gold/data_load_to_gold.sql
@@ -18,3 +18,21 @@ left join silver.erp_cust_az12 e
 on cu.cst_key = e.cid
 left join silver.erp_loc_a101 l
 on cu.cst_key = l.cid;
+
+create view gold.dim_products as 
+select 
+row_number() over(order by p.prd_start_dt, p.prd_key) as product_key,
+p.prd_id as product_id,
+p.prd_key as product_number,
+p.prd_nm as product_name,
+p.cat_id as category_id,
+pc.cat as category,
+pc.subcat as sub_category,
+pc.maintenance ,
+p.prd_cost as cost,
+p.prd_line as product_line,
+p.prd_start_dt as start_date
+from silver.crm_prd_info p
+left join silver.erp_px_cat_g1v2 pc
+on p.cat_id = pc.id
+where p.prd_end_dt is null
