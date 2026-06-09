d770fe78f9c902d37e22dbc8ae3040e31003ec59
Thiramdasu Shivasharan <213768851+Shiva-Sharan@users.noreply.github.com>
2026-06-09 18:26:40 +0530

    Update quality_checks_gold.sql

--- tests/quality_checks_gold.sql
+++ tests/quality_checks_gold.sql
@@ -10,3 +10,9 @@ SELECT product_id, count(*) from gold.dim_products
 group by product_id
 HAVING count(*) > 1;
 
+select * from gold.fact_sales f
+left join gold.dim_customers c
+on f.customer_key = c.customer_key
+left join gold.dim_products p
+on f.product_key = p.product_key
+where c.customer_key is null or p.product_number is null;
