5b03c94747481dceeb523423c91b4f7768095628
Thiramdasu Shivasharan <213768851+Shiva-Sharan@users.noreply.github.com>
2026-06-09 17:33:22 +0530

    Update quality_checks_gold.sql

--- tests/quality_checks_gold.sql
+++ tests/quality_checks_gold.sql
@@ -6,3 +6,7 @@ select customer_id,count(*) from gold.dim_customers
 group by customer_id
 having count(*) > 1;
 
+SELECT product_id, count(*) from gold.dim_products
+group by product_id
+HAVING count(*) > 1;
+
